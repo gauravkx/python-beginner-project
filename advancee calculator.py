e7b736693f9b606d7d45e68ad07b5ac7ae0412a8
gauravkx <gk9720419@gmail.com>
2025-11-08 16:16:20 +0530

    calculator and personal introduction programs
     Basic calculator 
                     - that adds two numbers  
                     - Personal intro generator that creates custom bios  
                     - Both built during first day of Python learning

---
+++ advancee calculator.py
@@ -0,0 +1,18 @@
+#adv calculator
+
+print("======ADVANCEE CALCULATOR======")
+
+num1 = float(input("enter num1"))
+num2 = float(input("enter num2"))
+
+print("HERE ARE RESULT")
+
+print("Addition is", round(num1+num2, 2))
+print("diffrence is",round(num1-num2, 2))
+print("multiplication is", round(num1*num2, 2))
+print("divison is", round(num1/num2, 2))
+print('floor divison is', round(num1//num2, 2))
+print("modulus is", round(num1%num2, 2))
+print("exponent is", round(num1**num2,2))
+
+print('THANKS FOR USING THIS CALCULATOR \n COME BACK AGAIN')
---
+++ intro generator.py
@@ -0,0 +1,16 @@
+ 
+
+#personal introdcution generator
+print("======personal intro======")
+
+name = input("enter your name")
+age = int(input("enter your age"))
+hobby = input("enter your hobby")
+city = input("enter your city")
+
+print("MY INTRO")
+
+print("Hello, \nI'm " + name + " !!")
+print("I'm", age, "years old and I live in" + city + ".")
+print("I love" + hobby + " in my free time.")
+print("Nice TO Meet You!!")
