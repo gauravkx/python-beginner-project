1a923de8d0250db6d5db792f17529f7bf9363245
gauravkx <gk9720419@gmail.com>
2025-11-09 21:03:28 +0530

    volume of sphere
    This help to find volume of shpere upto 3 decimal place

---
+++ volume of spheree.py
@@ -0,0 +1,11 @@
+
+
+# volume of sphere 
+
+print("==== volume of shphere===")
+r = float(input(" enter the value of r:"))
+volume =(4/3) * 3.14 * (r**3)
+
+print(" volume of sphere is ", round(volume, 3))
+
+
