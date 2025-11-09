0510093230dce5d28095db30aa97c72f33663781
gauravkx <gk9720419@gmail.com>
2025-11-09 21:07:45 +0530

    Celsius to  Fahrenheit
    simple program to find fahrenheit using value of celcius

---
+++ temp convertor.py
@@ -0,0 +1,10 @@
+# program to convert temp from celcius to fahrenhiet
+
+# formula is = (C*9/5)+ 32
+
+print("=== celcius to fahrenheit ===")
+
+C =float(input("enter the value of calcius"))
+F = (C*9/5) + 32
+
+print("TEMP IN FAHRENHEIT IS", F) 
