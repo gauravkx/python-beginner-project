4323ab2cf7d2b162531362a31fcac07824488043
gauravkx <gk9720419@gmail.com>
2025-11-11 13:06:50 +0530

    Update volume of spheree.py

--- volume of spheree.py
+++ volume of spheree.py
@@ -3,9 +3,10 @@
 # volume of sphere 
 
 print("==== volume of shphere===")
-r = float(input(" enter the value of r:"))
+r = float(input(" enter the value of r: "))
 volume =(4/3) * 3.14 * (r**3)
 
 print(" volume of sphere is ", round(volume, 3))
+
 
 
